6c0fd27231eb237821267e08e6200e22de71484e
Tom Hansen <tomh@uwm.edu>
2023-01-29 10:00:01 -0600

    update

--- collect.sh
+++ collect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rtl_433 -f 433.94M  -F json:data`date +%Y%m%d_%H%M%S`.json -M level -M noise -M time:utc -M protocol -M stats -Y autolevel -Y 
+rtl_433 -f 433.94M  -F json:data`date +%Y%m%d_%H%M%S`.json -M level -M noise -M time:utc -M protocol -M stats -Y autolevel 
 # rtl_433 -s 960000 -f 433.94M  -F json:data`date +%Y%m%d_%H%M%S`.json -M level -M noise -M time:utc -M protocol -M stats -Y autolevel
 
