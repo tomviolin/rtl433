bb2268f3598eed2dae4db51feb708c4abb13f695
Tom Hansen <tomh@uwm.edu>
2024-06-14 05:15:01 -0500

    update

--- collect868.sh
+++ collect868.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rtl_433 -s   2000000 -f 915.2M -F log -F json:data`date +%Y%m%d_%H%M%S`.json -M level -M noise -M time:utc -M protocol -M stats -Y autolevel 
+rtl_433 -s   200000 -f 915.2M -F log -F json:data`date +%Y%m%d_%H%M%S`.json -M level -M noise -M time:utc -M protocol -M stats -Y autolevel  -g 50
 # rtl_433 -s 960000 -f 433.90M  -F json:data`date +%Y%m%d_%H%M%S`.json -M level -M noise -M time:utc -M protocol -M stats -Y autolevel
 
